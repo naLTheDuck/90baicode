fd890eace48743e3607e8b01b70547ce65d99d17
naLTheDuck <62368256+naLTheDuck@users.noreply.github.com>
2025-11-12 14:59:47 +0700

    Refactor bai6.c to use double and return values

--- bai6.c
+++ bai6.c
@@ -2,38 +2,45 @@
 Cho số thực a và số tự nhiên n. Hãy viết chương trình có sử dụng hàm để tính
 
 a. a(a+1)...(a+n-1)
-b.  1/a+1/a(a+1)+...+1/a(a+1)...(a+n)
+b. 1/a+1/a(a+1)+...+1/a(a+1)...(a+n)
 */
 
 #include <stdio.h>
 #include <math.h>
 
-int caua(float a, int n) {
+double caua(double a, int n) {
 	int i;
-	float s=1;
+	double s=1;
 	for (i=0; i<n; i++) {
 		s=s*(a+i);
 	}
-	printf("%f", s);
-	return 0;
+	return s;
 }
 
-int caub(float a, int n) {
+double caub(float a, int n) {
 	int i;
-	float mau=a;
-	float s=0;
+	double mau=a;
+	double s=0;
 	for (i=0; i<=n; i++) {
-		s+=1/(1.0*mau);
-		mau*=a+i+1;
+		s+=1.0/mau;
+		mau*=(a+i+1);
 	}
-	printf("\n%f", s);
-	return 0;
+	return s;
 }
 
 int main() {
 	int n;
-	float a;
-	scanf("%f %d", &a, &n);
-	caua(a, n);
-	caub(a, n);
+	double a;
+	scanf("%lf %d", &a, &n);
+	double dapana = caua(a, n);
+	double dapanb = caub(a, n);
+	printf("%lf\n", dapana);
+	printf("%lf", dapanb);
 }
+
+/*
+    __
+___( o)> Quack!
+\ <_. )  
+ `---'   From naLTheDuck
+*/
